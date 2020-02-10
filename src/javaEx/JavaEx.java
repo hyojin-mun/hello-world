19d680e7e8dce7fd303d0d7a29cbf18c393e3244
C556 <C556@C556-PC>
2020-02-10 17:45:30 +0900

    2/10 update commit

--- src/javaEx/JavaEx.java
+++ src/javaEx/JavaEx.java
@@ -25,15 +25,75 @@ public class JavaEx {
 //		System.out.println("마지막 자릿수 : "+(x%10));
 //	}
 
-	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
-		System.out.print("반지름 : ");
-		double r =scanner.nextDouble();
-		final double PI=3.1416;
-		//구의 겉넓이(4*PI*r^2)
-		System.out.println("겉넓이 : "+(4*PI*r*r));
-		//구의 부피(4/3*PI*r^3)
-		System.out.println("부피 : "+(4/3.0*PI*r*r*r));
-	}
+//	public static void main(String[] args) {
+//		Scanner scanner=new Scanner(System.in);
+//		System.out.print("반지름 : ");
+//		double r =scanner.nextDouble();
+//		final double PI=3.1416;
+//		//구의 겉넓이(4*PI*r^2)
+//		System.out.println("겉넓이 : "+(4*PI*r*r));
+//		//구의 부피(4/3*PI*r^3)
+//		System.out.println("부피 : "+(4/3.0*PI*r*r*r));
+//	}
 	
+//	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		int x,y;
+//		System.out.print("x : ");
+//		x=scanner.nextInt();
+//		System.out.print("y : ");
+//		y=scanner.nextInt();
+//		if(x%y==0) {
+//			System.out.println("Y는 X의 약수이다.");
+//		}else {System.out.println("약수 아님");}
+//	}
+	
+//	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.print("값을 두 개 입력하라");
+//		double a=scanner.nextDouble();
+//		double b=scanner.nextDouble();
+//		System.out.println("a="+a+"\nb="+b);
+//		double max=a>b?a:b;
+//		System.out.println("큰 값은 "+max);
+//	}
+		
+//	public static void main(String[] args) {
+//		Scanner scanner=new Scanner(System.in);
+//		System.out.print("값 입력");
+//		int a=scanner.nextInt();
+//		int b=scanner.nextInt();
+//		System.out.println("a : "+a+"\nb : "+b);
+//		if (a<b) {
+//			int t=a;
+//			a=b;
+//			b=t;
+//		}
+//		System.out.println("a>=b가 되도록 내림차순 정렬 결과");
+//		System.out.println("a="+a+"\nb="+b);
+//	}
+	
+//	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		int n=scanner.nextInt();
+//		int m=scanner.nextInt();
+//		int p = 1;
+//        for(int j=0;j<n;j++){
+//            for (int i=0;i<m;i++){
+//                System.out.print(p+" "); // n*m 사각형 형태로 1 ~ n*m 까지의 숫자 출력
+//                p++;
+//            }
+//            System.out.println();
+//		}
+//	}
+	
+//	public static void main(String[] args) {
+//		Scanner scanner=new Scanner(System.in);
+//		int s= scanner.nextInt();
+//		int[] a= new int[s];
+//		
+//		for(int i=0;i<a.length;i++) {
+//			System.out.println(a[i]);
+//		}
+//	}
 }
